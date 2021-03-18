60c2f6df0f09eb6e7b4a435f1d7a66f6445b30a1
MattKD94 <55375247+MattKD94@users.noreply.github.com>
2021-03-18 20:22:59 +0100

    [CODE] Final Version

--- TP1.py
+++ TP1.py
@@ -4,27 +4,18 @@ Date : 28/01/2021
 Professor : MR. BLETZACKER
 """
 
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 #                        BIBLIOTHEQUES
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
 
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 #                          FONCTIONS
-#-----------------------------------------------------------
+# -----------------------------------------------------------
 
-""" def echanger(A, L1, L2): #échange les lignes L1 et L2 de la matrice
-    temp = A[L1]
-    A[L1] = A[L2]
-    A[L2] = temp
-
-def zero(A, L1, L2, k):
-    for i in range(len(A) + 1): # len(A) + 1 car la matrice est augmentée
-        A[L2][]
-"""
 # QUESTION 1
 
 def ReductionGauss(Aaug):
@@ -42,14 +33,13 @@ def ReductionGauss(Aaug):
         for k in range(i + 1, n):
             g = Aaug[k, i] / Aaug[i, i]
             for j in range(0, n):
-                Aaug[k, j] = Aaug[k, j] - g*Aaug[i, j]
-            Aaug[k, n] = Aaug[k, n] - g*Aaug[i, n]  
+                Aaug[k, j] = Aaug[k, j] - g * Aaug[i, j]
+            Aaug[k, n] = Aaug[k, n] - g * Aaug[i, n]
     return Aaug
 
 
-
 # QUESTION 2
- 
+
 def ResolutionSystTriSup(Taug):
     """
    Cette fonction renvoie la solution d’un système T X = B, où T est triangulaire supérieure. 
@@ -58,17 +48,16 @@ def ResolutionSystTriSup(Taug):
 
       Retourne: La solution du système TX = B
     """
-        
+
     n, m = np.shape(Taug)
     x = np.zeros(n)
-    x[n - 1] = Taug[n - 1, m - 1]/Taug[n - 1, n - 1]
-    for i in range (n - 2, -1, -1):
+    x[n - 1] = Taug[n - 1, m - 1] / Taug[n - 1, n - 1]
+    for i in range(n - 2, -1, -1):
         x[i] = Taug[i, m - 1]
-        for j in range (i + 1, n): 
-            x[i] = x[i] - Taug[i, j]*x[j]
-        x[i] = x[i]/Taug[i, i]
+        for j in range(i + 1, n):
+            x[i] = x[i] - Taug[i, j] * x[j]
+        x[i] = x[i] / Taug[i, i]
     return x
-
 
 
 # QUESTION 3
@@ -83,19 +72,14 @@ def Gauss(A, B):
      Retourne: La solution du système AX = B
 
     """
-        
+
     n, m = np.shape(A)
     R = np.c_[A, B]
     return ResolutionSystTriSup(ReductionGauss(R))
-"""
-A = np.array([[2,5,6], [4,11,9], [-2,-8,7]])
-B = np.array([7,12,3])
-print(Gauss(A, B))
-"""
-
 
 
 # -----------------------DECOMPOSITION LU--------------------------
+
 
 # QUESTION 1
 
@@ -108,27 +92,18 @@ def DecompostionLU(A):
      Retourne: La décomposition LU de A
 
     """
-        
+
     n, m = np.shape(A)
     L = np.eye(n, m)
     for i in range(0, n):
         for k in range(i + 1, n):
             g = A[k, i] / A[i, i]
             for j in range(0, n):
-                A[k, j] = A[k, j] - g*A[i, j]
+                A[k, j] = A[k, j] - g * A[i, j]
             L[k, i] = g
 
     return L, A
 
-
-# A = np.array([[3,2,-1,4], [-3,-4,4,-2], [6,2,2,7], [9,4,2,18]])
-# L, U = DecompostionLU(A)
-
-"""
-print(L)
-print(A)
-print(L.dot(U))
-"""
 
 # QUESTION 2
 
@@ -142,26 +117,23 @@ def ResolutionLU(L, U, B):
            
     Retourne: La solution de l'équation AX = B
     """
-        
+
     n, m = np.shape(L)
     Y = np.zeros(n)
     X = np.zeros(n)
     for i in range(0, n):
         S = 0
         for k in range(0, i):
-            S += L[i, k]*Y[k]
+            S += L[i, k] * Y[k]
         Y[i] = B[i] - S
 
     for j in range(n - 1, -1, -1):
-        S=0
+        S = 0
         for k in range(j, n):
-            S += U[j, k]*X[k]
-        X[j] = (Y[j] - S) / U[j,j]
+            S += U[j, k] * X[k]
+        X[j] = (Y[j] - S) / U[j, j]
     return X
 
-# B = np.array([4,-5,-2,13])
-
-#print(ResolutionLU(L, U, B))
 
 # -----------------------Variantes de l’algorithme de Gauss--------------------------
 
@@ -273,7 +245,10 @@ Cette fonction rend la solution d’un système AX = B avec la méthode de Gauss
 
     return np.array(X_ordonne)
 
-# QUESTION 4 AFFICHAGE DES GRAPHES
+
+# QUESTION 4
+# AFFICHAGE DES GRAPHES
+
 TpsG = []
 TpsLin = []
 TpsLU = []
@@ -288,102 +263,98 @@ ErreurLU = []
 ErreurPP = []
 ErreurPT = []
 
-
 for n in range(100, 500, 100):
     A = np.random.rand(n, n)
     B = np.random.rand(n, 1)
 
-#Gauss
+    # Gauss
 
     time_startG = time.time()
     X1 = Gauss(A, B)
     time_endG = time.time()
     TpsG.append(time_endG - time_startG)
-    Y1 = np.linalg.norm(np.dot(A, X1) - B)/np.linalg.norm(B)
+    Y1 = np.linalg.norm(np.dot(A, X1) - B) / np.linalg.norm(B)
     ErreurG.append(Y1)
 
-# #Linalg
-#
-#     time_startLin = time.time()
-#     X2 = np.linalg.solve(A, B)
-#     time_endLin = time.time()
-#     TpsLin.append(time_endLin - time_startLin)
-#     Y2 = np.linalg.norm(np.dot(A, X2) - B)/np.linalg.norm(B)
-#     ErreurLin.append(Y2)
+    # Linalg
 
-#LU
+    time_startLin = time.time()
+    X2 = np.linalg.solve(A, B)
+    time_endLin = time.time()
+    TpsLin.append(time_endLin - time_startLin)
+    Y2 = np.linalg.norm(np.dot(A, X2) - B) / np.linalg.norm(B)
+    ErreurLin.append(Y2)
+
+    # LU
 
     time_startLU = time.time()
     L, U = DecompostionLU(A)
     X3 = ResolutionLU(L, U, B)
     time_endLU = time.time()
     TpsLU.append(time_endLU - time_startLU)
-    Y3 = np.linalg.norm(np.dot(A, X3) - B)/np.linalg.norm(B)
+    Y3 = np.linalg.norm(np.dot(A, X3) - B) / np.linalg.norm(B)
     ErreurLU.append(Y3)
-#
-# #Pivot Partiel
-#
-#     time_startPP = time.time()
-#     X4 = GaussChoixPivotPartiel(A, B)
-#     time_endPP = time.time()
-#     TpsPP.append(time_endPP - time_startPP)
-#     Y4 = np.linalg.norm(np.dot(A, X4) - B)/np.linalg.norm(B)
-#     ErreurPP.append(Y4)
-#
-# #Pivot Total
-#
-#     time_startPT = time.time()
-#     X5 = GaussChoixPivotTotal(A, B)
-#     time_endPT = time.time()
-#     TpsPT.append(time_endPT - time_startPT)
-#     Y5 = np.linalg.norm(np.dot(A, X5) - B)/np.linalg.norm(B)
-#     ErreurPT.append(Y5)
 
-#Taille de la matrice
+    # Pivot Partiel
+
+    time_startPP = time.time()
+    X4 = GaussChoixPivotPartiel(A, B)
+    time_endPP = time.time()
+    TpsPP.append(time_endPP - time_startPP)
+    Y4 = np.linalg.norm(np.dot(A, X4) - B) / np.linalg.norm(B)
+    ErreurPP.append(Y4)
+
+    # Pivot Total
+
+    time_startPT = time.time()
+    X5 = GaussChoixPivotTotal(A, B)
+    time_endPT = time.time()
+    TpsPT.append(time_endPT - time_startPT)
+    Y5 = np.linalg.norm(np.dot(A, X5) - B) / np.linalg.norm(B)
+    ErreurPT.append(Y5)
+
+    # Taille de la matrice
 
     length.append(n)
 
-#PLOT
+# PLOT
 
 plt.figure("Temps d'exécution en fonction de la taille de la matrice")
-plt.plot(length, TpsG, label = "Gauss")
-# plt.plot(length, TpsLin, label = "Linalg")
-plt.plot(length, TpsLU, label = "LU")
-# plt.plot(length, TpsPP, label = "Pivot Partiel")
-# plt.plot(length, TpsPT, label = "Pivot Total")
+plt.plot(length, TpsG, label="Gauss")
+plt.plot(length, TpsLin, label="Linalg")
+plt.plot(length, TpsLU, label="LU")
+plt.plot(length, TpsPP, label="Pivot Partiel")
+plt.plot(length, TpsPT, label="Pivot Total")
 plt.xlabel("Taille de la matrice(n)")
 plt.ylabel("Temps d'execution(s)")
 plt.title("Temps d'exécution en fonction de la taille de la matrice")
 plt.grid()
 plt.legend()
 
-#ERREURS
+# ERREURS
 
-# plt.figure("Courbe d'erreurs")
-# plt.title("Erreur relative en fonction de la taille de la matrice")
-# plt.plot(length, ErreurG, label = "Gauss")
-# plt.plot(length, ErreurLU, label = "LU")
-# plt.plot(length, ErreurPP, label = "Pivot Partiel")
-# plt.plot(length, ErreurPT, label = "Pivot Total")
-# plt.yscale(u'log')
-# plt.xlabel("Taille de la matrice(n)")
-# plt.ylabel("Erreur relative")
-# plt.grid()
-# plt.legend()
+plt.figure("Courbe d'erreurs")
+plt.title("Erreur relative en fonction de la taille de la matrice")
+plt.plot(length, ErreurG, label="Gauss")
+plt.plot(length, ErreurLU, label="LU")
+plt.plot(length, ErreurPP, label="Pivot Partiel")
+plt.plot(length, ErreurPT, label="Pivot Total")
+plt.yscale(u'log')
+plt.xlabel("Taille de la matrice(n)")
+plt.ylabel("Erreur relative")
+plt.grid()
+plt.legend()
 
+# GRAPHES LOLGLOG
 
-#GRAPHES LOLGLOG
-
-# plt.figure("Graphes loglog")
-# plt.loglog(length, TpsG, label = "Gauss")
-# plt.loglog(length, TpsLU, label = "LU")
-# plt.loglog(length, TpsPP, label = "Pivot Partiel")
-# plt.loglog(length, TpsPT, label = "Pivot Total")
-# plt.title("Temps d'exécution en fonction de la taille de la matrice")
-# plt.xlabel("Taille de la matrice(n)")
-# plt.ylabel("Temps d'exécution(s)")
-# plt.grid()
-# plt.legend()
+plt.figure("Graphes loglog")
+plt.loglog(length, TpsG, label="Gauss")
+plt.loglog(length, TpsLU, label="LU")
+plt.loglog(length, TpsPP, label="Pivot Partiel")
+plt.loglog(length, TpsPT, label="Pivot Total")
+plt.title("Temps d'exécution en fonction de la taille de la matrice")
+plt.xlabel("Taille de la matrice(n)")
+plt.ylabel("Temps d'exécution(s)")
+plt.grid()
+plt.legend()
 plt.show()
-
-
